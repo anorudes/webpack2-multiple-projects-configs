b9c8d88a4085f16aedbfa3065618d8fcf374da13
anorudes <anorudes@gmail.com>
2017-05-04 17:38:04 +0500

    Add simple server.js

---
+++ bin/test.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const config = JSON.parse(fs.readFileSync('./.babelrc'));
+const hook = require('css-modules-require-hook');
+const sass = require('node-sass');
+
+hook({
+  generateScopedName: process.env.NODE_ENV === 'production'
+    ? '[hash:base64:5]'
+    : '[path]__[local]__[hash:base64:3]',
+  extensions: ['.mscss'],
+  preprocessCss: (data, filename) =>
+    sass.renderSync({
+      data,
+      file: filename,
+    }).css,
+});
+
+// Enable ignore scss and remove prop-types
+const ignore = [
+  [
+    'babel-plugin-transform-require-ignore', {
+      extensions: ['.scss'],
+    },
+  ],
+  'react-remove-prop-types',
+];
+
+config.plugins = config.plugins.concat(ignore);
+require('babel-core/register')(config);
+require('asset-require-hook')({
+  extensions: ['jpg', 'png', 'svg'],
+});
+
+global.__CLIENT__ = false;
+
+require('../src/test/setup.js');
